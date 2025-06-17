4ca6bfb947102a6cbb73a5a694f23d0a38c281c0
sievmaylorn <sievmaylorn04@gmail.com>
2025-06-17 19:27:11 +0700

    image commited

--- src/components/BestDeal/CashCard.jsx
+++ src/components/BestDeal/CashCard.jsx
@@ -2,24 +2,28 @@ import React from 'react';
 
 function CashCard() {
   return (
-    <div  className="w-full overflow-hidden  bg-pink-500  py-5">
-      <div className="max-w-5xl mx-auto px-6 flex flex-col md:flex-row items-start lg:gap-10 ">
+    <div className="w-full overflow-hidden bg-pink-500 py-5">
+      <div className="max-w-5xl mx-auto px-6 flex flex-col md:flex-row items-start lg:gap-10">
         {/* Left Content */}
         <div className="w-full md:w-1/2 flex flex-col items-start text-left">
-          <h1 className="text-3xl lg:ms-15 md:ms-10 sm:text-4xl md:text-5xl lg:text-6xl font-bold text-white">
-             Get 5% Cash back
+          <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-white">
+            Get 5% Cash back
           </h1>
-          <p className="text-lg lg:ms-15 md:ms-10 sm:text-xl md:text-2xl mt-4 text-white">
+          <p className="text-lg sm:text-xl md:text-2xl mt-4 text-white">
             on Shopcart.com
           </p>
-          <button className="mt-5 lg:ms-15 md:ms-10 px-6 py-3 text-white text-lg md:text-2xl bg-pink-700 rounded-4xl hover:bg-pink-500 transition duration-300 ease-in-out">
+          <button className="mt-5 px-6 py-3 text-white text-lg md:text-2xl bg-pink-700 rounded-4xl hover:bg-pink-600 transition duration-300 ease-in-out">
             Learn More
           </button>
         </div>
 
-        {/* Right Image / Placeholder */}
-        <div className="w-full md:w-1/2">
-          <img src="/Cashcard.png" alt="No-image" className="hidden md:block md:w-[100%] md:h-auto lg:w-[30vw] lg:h-[auto] object-contain" />
+        {/* Right Image */}
+        <div className="w-full md:w-1/2 hidden sm:block">
+          <img
+            src="/Cashcard.png"
+            alt="No-image"
+            className="w-full h-auto lg:w-[30vw] object-contain"
+          />
         </div>
       </div>
     </div>
